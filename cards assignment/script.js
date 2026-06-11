5f39e36950e40972fdb095ac8b3325db5b29458a
VictorXyZy <vin5mungereti@gmail.com>
2026-06-12 00:55:20 +0300

    responsiveness creation

--- cards assignment/script.js
+++ cards assignment/script.js
@@ -1,17 +1,23 @@
-function changeIcon(icon) {
-  icon.classList.toggle("fa-regular");
-  icon.classList.toggle("fa-solid");
-  icon.style.color = icon.classList.contains("fa-solid") ? "#ff3b30" : "#444";
+function addToCart(btn) {
+    const isAdded = btn.classList.toggle("added");
+
+    if (isAdded) {
+        btn.innerHTML = '<i class="fa-solid fa-circle-check"></i> Added to cart';
+        btn.style.backgroundColor = "#28a745";
+        btn.style.color = "white";
+    } else {
+        btn.innerHTML = '<i class="fa-solid fa-cart-shopping"></i> Add to cart';
+        btn.style.backgroundColor = "var(--btn-color)";
+        btn.style.color = "white";
+    }
 }
 
-function addToCart(btn) {
-  const isAdded = btn.classList.toggle("added");
+function changeIcon(icon) {
+    const isLiked = icon.classList.toggle("active");
 
-  if (isAdded) {
-    btn.innerHTML = '<i class="fa-solid fa-circle-check"></i> Added to cart';
-    btn.style.backgroundColor = "#28a745";
-  } else {
-    btn.innerHTML = '<i class="fa-solid fa-cart-shopping"></i> Add to cart';
-    btn.style.backgroundColor = "var(--btn-color)";
-  }
+    if (isLiked) {
+        icon.classList.replace("fa-regular", "fa-solid");
+    } else {
+        icon.classList.replace("fa-solid", "fa-regular");
+    }
 }
